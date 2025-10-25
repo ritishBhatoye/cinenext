469c9509065f2c09760e148e6115fc7a9fcb1280
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-26 03:16:22 +0530

    task-4-home-fix the home screen

--- src/components/landing/MediaRowSlider/MediaRowItem.tsx
+++ src/components/landing/MediaRowSlider/MediaRowItem.tsx
@@ -7,12 +7,12 @@ interface props {
 const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
 const MediaRowItem = ({ data }: props) => {
   return (
-    <Link href={`/play/${data?.id}`}>
+    <Link href={`/play/${data?.id}`} className="flex-shrink-0">
       <Image
         src={`${TMDB_IMAGE_BASE}${data?.poster_path}`}
         alt={`No Video for ${data?.popularity}`}
-        width={300}
-        height={400}
+        width={210}
+        height={140}
         className="rounded-lg"
       />
     </Link>
