823f4c2e3774856a364fddc97b1e55ddc48c365d
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-28 00:23:56 +0530

    Links

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,4 +2,4 @@
 export { default as Text } from "./Text";
 export { default as Icon } from "./Icon";
 export { default as Image } from "./Image";
-export { default as Link } from "./Link";
+export { default as Link } from "./Links";
