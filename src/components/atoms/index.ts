d789392f50dcb38aa020254865b9110488402169
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-28 00:24:27 +0530

    Images

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,5 @@
 // Atoms - Single HTML tag components
 export { default as Text } from "./Text";
 export { default as Icon } from "./Icon";
-export { default as Image } from "./Image";
+export { default as Image } from "./Images";
 export { default as Link } from "./Link";
