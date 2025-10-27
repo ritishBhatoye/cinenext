49362355ae48297485a92e1c4fde1a5b68a5416a
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-28 00:23:30 +0530

    Icon

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,5 @@
 // Atoms - Single HTML tag components
 export { default as Text } from "./Text";
-export { default as Icon } from "./Icons";
+export { default as Icon } from "./Icon";
 export { default as Image } from "./Image";
 export { default as Link } from "./Link";
