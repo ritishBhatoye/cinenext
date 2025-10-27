2275b032d2972cdf36b409867df33b7de1e02580
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-27 22:39:01 +0530

    task-6-search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@
 import SearchBar from "@/components/global/SearchBar";
 import MediaItem from "@/components/landing/MediaRowSlider/MediaItem";
 import { useTodayTrending } from "@/hooks/useTodayTrending";
-
+import { Text } from "@/components/atoms/index";
 const SearchPage = () => {
   const { combined } = useTodayTrending();
 
@@ -16,11 +16,22 @@ const SearchPage = () => {
             <SearchBar />
           </div>
         </div>
+        <div className="flex flex-col items-start gap-3 pb-4">
+          <div className="flex-row flex items-center gap-2">
+            <div className="w-2 h-10 rounded-full bg-linear-to-b from-red-800 via-red-500 to-red-400"></div>
 
+            <Text as="h3" className=" text-2xl   text-white font-bold">
+              Trending Today
+            </Text>
+          </div>
+          <Text as="p" className="text-gray-400">
+            {combined?.data?.length} results found
+          </Text>
+        </div>
         {/* Movies Grid */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
           {combined?.data?.map((media, index: number) => (
-            <div key={index} className="aspect-[2/3] relative">
+            <div key={index} className="aspect-2/3 relative">
               <MediaItem data={media} />
             </div>
           ))}
